56f71c20af83125e6a5da86824ffff19c893fd1e
sofyanux <ssauri30@gmail.com>
2024-05-07 15:23:56 +0800

    first commit

---
+++ src/books.js
@@ -0,0 +1,4 @@
+"use strict";
+let books = [];
+
+module.exports = books;
---
+++ src/routes.js
@@ -0,0 +1,42 @@
+"use strict";
+const {
+  addBookHandler,
+  getAllBooksHandler,
+  getBookByIdHandler,
+  editBookByIdHandler,
+  deleteBookByIdHandler
+} = require("./handlers");
+
+const routes = [
+  {
+    method: "POST",
+    path: "/books",
+    handler: addBookHandler
+  },
+
+  {
+    method: "GET",
+    path: "/books",
+    handler: getAllBooksHandler
+  },
+
+  {
+    method: "GET",
+    path: "/books/{id}",
+    handler: getBookByIdHandler
+  },
+
+  {
+    method: "PUT",
+    path: "/books/{id}",
+    handler: editBookByIdHandler
+  },
+
+  {
+    method: "DELETE",
+    path: "/books/{id}",
+    handler: deleteBookByIdHandler
+  }
+];
+
+module.exports = routes;
